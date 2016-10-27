3880368ebd9d04f337ef7454506675384ef5da11
podiki <podiki@users.noreply.github.com>
2016-10-27 11:51:51 +0200

    use exiftool's arg file for copying to iptc, added more comments

--- reverse_geo.py
+++ reverse_geo.py
@@ -9,29 +9,31 @@ parser = argparse.ArgumentParser(description='Simple reverse geocoding with geop
 parser.add_argument('files', nargs='+', help='files to reverse geocode')
 files = parser.parse_args().files
 
+# Use Nominatim, from OpenStreetMap, for reverse lookup
 geolocator = Nominatim()
 
 with exiftool.ExifTool() as et:
     for f in files:
+        # Find place names from GPS already in file
         gps = et.get_tag('GPSLatitude', f), et.get_tag('GPSLongitude', f)
         location = geolocator.reverse(gps).raw['address']
-        creator = et.get_tag('Creator', f)
-        copyright = et.get_tag('Copyright', f)
-        keywords = ', '.join(et.get_tag('XMP-dc:Subject', f))
-        title = et.get_tag('XMP-dc:Title')
-        description = et.get_tag('XMP-dc:Description', f)
 
-        # Make sure something is written in IPTC
-        # so that the MWG tags write to IPTC as well
-        params = map(fsencode, ["-IPTC:By-line=nobody should see this", f])
-        et.execute(*params)
-        
-        params = map(fsencode, ["-MWG:Creator=" + creator,
-                                "-MWG:Copyright=" + copyright,
-                                "-MWG:Keywords=" + keywords,
-                                "-MWG:Country=" + location['country'],
+        # Copy XMP info to IPTC (darktable does not write IPTC currently),
+        # using xmp2iptc.args from exiftool's complete Perl distribution
+        # This also ensures that the MWG tags write to IPTC as well
+        with exiftool.ExifTool() as et_iptc:
+            params = map(fsencode, ["-@", "xmp2iptc.args", f])
+            print(et_iptc.execute(*params).decode('utf-8')) # output comes as byte
+
+        # Apply reverse geocoding using MWG tag standards
+        # (see, e.g., http://www.metadataworkinggroup.org/ and
+        # http://www.sno.phy.queensu.ca/~phil/exiftool/TagNames/MWG.html)
+        # This is a preliminary try, may vary based on country, region, etc.,
+        # so will have to manually monitor and adjust in the future,
+        # especially the Location tag
+        params = map(fsencode, ["-MWG:Country=" + location['country'],
                                 "-MWG:State=" + location['state'],
                                 "-MWG:City=" + location['city'],
                                 "-MWG:Location=" + location['city_district'],
                                 f])
-        et.execute(*params)
+        print(et.execute(*params).decode('utf-8'))
